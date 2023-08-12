aac28cc21586d1c36657588b66d5e013250ef5f9
Luca Saverio Esposito <esposito.1917959@studenti.uniroma1.it>
2023-08-12 12:47:01 +0200

    V2 of the program, now works with GRPC. Still some arrow doesn't print well

--- main/masterMain.go
+++ main/masterMain.go
@@ -17,7 +17,7 @@ import (
 
 func main() {
 	start := time.Now()
-	utils.CreateRandomGraph(10, 5, 3)
+	utils.CreateRandomGraph(20, 5, 3)
 
 	graph := utils.Convert(constants.GraphPath)
 	if graph == nil {
