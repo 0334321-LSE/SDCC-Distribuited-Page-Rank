10b69b7ec83aefb55c7f2e4271fb2470073179a5
Luca Saverio Esposito <esposito.1917959@studenti.uniroma1.it>
2023-08-12 12:56:23 +0200

    Starting to use container, now the program works with containers but there are only 3 of them, one for each main.

---
+++ Mapper/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"Mapper/mapper"
+	"google.golang.org/grpc"
+	"log"
+	"net"
+)
+
+func main() {
+	lis, err := net.Listen("tcp", ":9000")
+	if err != nil {
+		log.Fatalf("Failed to listen on port 9000 %v", err)
+	}
+	log.Printf("Listening on port 9000 \n")
+
+	mapperServer := mapper.Mapper{}
+
+	// Initialize gRPC server for Mapper
+	grpcServer := grpc.NewServer()
+
+	mapper.RegisterMapperServer(grpcServer, &mapperServer)
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("Failed to serve gRPC on port 9000 %v", err)
+
+	}
+}
---
+++ Reducer/reducer/reducer.go
@@ -0,0 +1,54 @@
+package reducer
+
+import (
+	"Reducer/constants"
+	"context"
+	"errors"
+	"log"
+)
+
+type Reducer struct {
+}
+
+func (reducer *Reducer) mustEmbedUnimplementedReducerServer() {
+	//TODO implement me
+	panic("implement me")
+}
+
+// Reduce ->  sum aggregated page rank values and return the value
+func (reducer *Reducer) Reduce(ctx context.Context, input *ReducerInput) (*ReducerOutput, error) {
+
+	if input != nil {
+		accumulator := float32(0.0)
+		for _, rank := range input.PageRankShares {
+			accumulator += rank
+		}
+
+		newPageRank := float32((1.0-constants.DampingFactor)/float64(input.GraphSize)) + constants.DampingFactor*accumulator
+		log.Printf("\nNodeID: %d -> evaluated page rank: %f", input.NodeId, newPageRank)
+
+		return &ReducerOutput{
+			NodeId:       input.NodeId,
+			NewRankValue: newPageRank,
+		}, nil
+	} else {
+
+		return nil, errors.New("input isn't valid")
+	}
+
+}
+
+// ReduceCleanUp -> use the cleanUp formula to fix page rank value
+func (reducer *Reducer) ReduceCleanUp(ctx context.Context, input *ReducerCleanUpInput) (*ReducerOutput, error) {
+	if input != nil {
+		massShares := input.SinkMass / float32(input.GraphSize)
+		newPageRank := float32((1.0-constants.DampingFactor)/float64(input.GraphSize)) + constants.DampingFactor*(input.CurrentPageRank+massShares)
+		return &ReducerOutput{
+			NodeId:       input.NodeId,
+			NewRankValue: newPageRank,
+		}, nil
+	} else {
+
+		return nil, errors.New("input isn't valid")
+	}
+}
